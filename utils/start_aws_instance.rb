e20ec146de8fdfb80d053b0a64b92cbcbb8d72cf
Barry Byrne <barry.j.byrne@gmail.com>
2012-06-07 22:31:45 +0100

    Add utils dir and start_aws_instance.rb

---
+++ utils/start_aws_instance.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'right_aws'
+require 'yaml'
+
+creds = YAML::load( File.open( '/etc/cloudtools/cloud_creds.yml' ) )
+
+server_name="archiver_db"
+server_creator="bb"
+
+aws_key=creds["aws_key"];
+aws_secret=creds["aws_secret"];
+
+aws_id="i-36cd2a4fXXX"
+
+ec2 = RightAws::Ec2.new(
+	aws_key,
+	aws_secret,
+  	{:logger => Logger.new('/tmp/ec2.log')}
+  	)
+  	
+ARGV.each do|i|
+	# Assume each argument is an instance ID
+	# Check that it exists and is in a stopped state
+	
+	matching_instances = ec2.describe_instances(
+		:filters => {
+			'instance-state-code' => 80,
+			'instance-id' => i
+			})
+			
+	if matching_instances.count == 1
+		puts "Starting instance #{i}"
+		ec2.start_instances(i)
+	else
+		puts "Did NOT find stopped instance #{i}"
+	end
+end
