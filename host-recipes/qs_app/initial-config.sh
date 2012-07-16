a909085f2b3185135b8025679c31dd5b29e379b3
Barry Byrne <barry.j.byrne@gmail.com>
2012-07-16 20:46:06 +0100

    Fix typo in deploy key

---
+++ host-recipes/qs_app/initial-config.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# The script updates installed packages and adds any required ones for backing up the RDS server.
+# Some initial config, including installing git and the cloud credentials needs to be done prior to this
+# ideally, by using the user_data file for amazon cloud servers
+
+
+LOGFILE="/tmp/setup.log"
+
+(
+
+	# Prevent ssh timeouts for routers that dump idle sessions too quickly
+	echo "ClientAliveInterval 60" >> /etc/ssh/sshd_config
+	/etc/init.d/sshd restart
+	
+	echo "Initial Host Setup - $(date -R)!"
+	# Don't bother installing gem docs
+	echo "gem: --no-ri --no-rdoc" >> ~/.gemrc
+	
+	SCRIPT_DIR=/usr/local/scripts
+	CT_PATH=$REPOS_PATH/cloudtools
+	
+	mkdir -p "$SCRIPT_DIR/qs_app"
+	for f in $(ls -d /usr/local/repos/cloudtools/host-recipes/qs_app/scripts/*); do
+		ln -s $f /usr/local/scripts/qs_app;
+	done
+		
+	# Add relevant ssh public keys for access	
+	cat /usr/local/repos/cloudtools/public_keys/bb_*id_rsa.pub >> ~ec2-user/.ssh/authorized_keys
+	
+	# Remove ls coloring
+	echo "unalias ls" >> ~ec2-user/.bash_profile
+	
+	yum -y update
+	
+	yum -y install rubygems
+	gem update --system
+	
+	# The following are required for the nokogiri gem
+	yum install -y gcc make ruby-devel libxml2 libxml2-devel libxslt libxslt-devel
+	
+	gem install right_aws
+	
+
+	
+	echo "Initial Setup Done -  $(date -R)!"
+	
+) >> ${LOGFILE}
---
+++ host-recipes/qs_app/scripts/refresh-scripts.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Grab latest version of scripts
+
+cd /usr/local/repos/cloudtools
+/usr/bin/git pull
